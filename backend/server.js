40d89881f567abe1c84ae4fc56b35e703c616354
Sai Saketh <141397642+SakethCodesFS@users.noreply.github.com>
2024-06-03 01:53:59 -0500

    minor updates

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,17 @@ const port = process.env.PORT || 5001;
 console.log(`Bucket Name: ${process.env.BUCKET_NAME}`);
 
 app.use(bodyParser.json());
-app.use(cors());
+const corsOptions = {
+  origin: 'https://cookmartai.onrender.com',
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
 
 app.post('/process-video', async (req, res) => {
   console.log('Received /process-video request');
